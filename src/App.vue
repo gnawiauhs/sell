0d115e18cfa91161a09af480025d62ca3e0bda9f
WangShuai <hansmking@163.com>
2018-03-04 19:38:20 +0800

    'sell'

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+<div >
+  <v-header :seller="seller"></v-header>
+  <div class="tab border-1px">
+    <router-link to="/goods" class="tab-item">商品</router-link>
+    <router-link to="/ratings" class="tab-item">评价</router-link>
+    <router-link to="/seller" class="tab-item">商家</router-link>
   </div>
+  <router-view></router-view>
+</div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script type="text/ecmascript-6">
+  import header from 'components/header/header.vue'
+
+  const ERR_OK = 0
+  export default {
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.seller = response.data
+        }
+      })
+    },
+    components: {
+      'v-header': header
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      display: block
+      text-align: center
+      font-size: 14px
+      color: rgb(77, 85, 93)
+      &.active
+        color: rgb(240, 20, 20)
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
 
 Vue.use(Router)
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/goods'
+    },
+    {
+      path: '/goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      component: Seller
     }
   ]
 })
